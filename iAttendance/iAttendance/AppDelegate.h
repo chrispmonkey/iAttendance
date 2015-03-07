423d863ced7e54499071268120cd11643ea2acdd
Christopher L. Price <chrispmonkey@gmail.com>
2015-03-07 00:41:25 -0500

    Initial Commit

---
+++ iAttendance/iAttendance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAttendance
+//
+//  Created by Christopher L. Price on 3/7/15.
+//  Copyright (c) 2015 Christopher L. Price. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
